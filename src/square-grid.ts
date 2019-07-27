02c6d6b91e9540e87cda26b00289d523c6e88d18
alexddhuang <alexddhuang@gmail.com>
2019-07-27 13:42:26 +0800

    SquareGrid

---
+++ src/square-grid.ts
@@ -0,0 +1,57 @@
+import { Node, Graph } from "./graph";
+
+export class Location implements Node {
+    public x: number;
+    public y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+export class SquareGrid implements Graph {
+    private width: number;
+    private height: number;
+    private walls: Location[];
+
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+        this.walls = [];
+    }
+
+    public inBounds(location: Location): boolean {
+        const x = location.x;
+        const y = location.y;
+        return 0 <= x && x < this.width && 0 <= y && y < this.height;
+    }
+
+    public passable(location: Location): boolean {
+        const x = location.x;
+        const y = location.y;
+        for (let brick of this.walls) {
+            if (brick.x == x && brick.y == y)
+                return false;
+        }
+        return true;
+    }
+
+    public neighbors(node: Node): Node[] {
+        const location = <Location>node;
+        const x = location.x;
+        const y = location.y;
+
+        let results = [
+            new Location(x-1,y),
+            new Location(x,y-1),
+            new Location(x+1,y),
+            new Location(x,y+1),
+        ];
+
+        results = results.filter(loc => this.inBounds(loc));
+        results = results.filter(loc => this.passable(loc));
+
+        return results;
+    }
+}
